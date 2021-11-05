53f95afb100f645c55023bce0714b35e965c5026
ericmlyons <edub187@yahoo.com>
2021-11-04 22:31:33 -0400

    Re-commit

---
+++ src/components/homedefault/banner.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {useStaticQuery, graphql} from 'gatsby';
+import Img from "gatsby-image";
+
+const Banner = () => {
+    const banenrQueryData = useStaticQuery (graphql`
+        query BannerDefaultQuery {
+                homedefaultJson(id: {eq: "main-banner"}) {
+                title    
+                subtitle
+                bgImage {
+                    childImageSharp {
+                        fluid(quality: 100, maxWidth: 1920, maxHeight: 850) {
+                            ...GatsbyImageSharpFluid_withWebp
+                            presentationHeight
+                            presentationWidth
+                        }
+                    }
+                }
+            },
+            file(relativePath: {eq: "images/banner/bg-image-02.jpg"}) {
+                childImageSharp {
+                  fixed (quality: 100, width: 1250, height: 950) {
+                    ...GatsbyImageSharpFixed
+                  }
+                }
+            }
+        }
+    `);
+      
+    // const BannerImages = banenrQueryData.homedefaultJson.bgImage.childImageSharp.fluid;
+    const PortfolioImages = banenrQueryData.file.childImageSharp.fixed;
+    const Title = banenrQueryData.homedefaultJson.title;
+    const SubTitle = banenrQueryData.homedefaultJson.subtitle;
+
+    return (
+        <div className="rn-slider-area" id="home">
+            {/* Start Single Slider  */}
+            <div className="rn-slide slider-style-01 banner-fixed-height">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-xl-6">
+                            <div className="inner">
+                                <div className="content text-left">
+                                    <h1 className="title" dangerouslySetInnerHTML={{ __html: Title }}></h1>
+                                    <h4 className="subtitle" dangerouslySetInnerHTML={{ __html: SubTitle }}></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* End Single Slider  */}
+            <div className="thumbnail">
+                <div className="thumbnail-inner">
+                    <Img className="portfolio-images" fixed={PortfolioImages} />
+                </div>
+            </div>
+           
+        </div>
+    )
+}
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,29 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Banner from "../components/homedefault/banner";
+import About from "../components/homedefault/about";
+import Project from "../components/homedefault/project";
+import Testimonial from "../components/homedefault/testimonial";
+import Service from "../components/homedefault/service";
+import Brand from "../components/homedefault/brand";
+import BlogPost from "../components/blogPost";
+import Contact from "../elements/contact/contact";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const IndexPage = () => (
+const Index = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <SEO title="Waxon" />
+    <Banner />
+    <About />
+    <Service />
+    <div className="portfolio-id" id="portfolio">
+      <Project />
+      <Brand />
+      <Testimonial />
+    </div>
+    <BlogPost />
+    <Contact />
   </Layout>
 )
-
-export default IndexPage
+export default Index;
